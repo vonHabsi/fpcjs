f0e80ad7d7db741350a188d8d90ea3ac13c4a402
Dusan.Halicky <Dusan.Halicky@15cd7b3e-bbfe-11de-ad03-79fce359684b>
2010-08-31 09:28:22 +0000

    Added simple javascript command line calculator, clone of BC

---
+++ trunk/src/jc.dpr
@@ -0,0 +1,51 @@
+program jc;
+
+{*
+Experimental Javascript Calculator for command line
+}
+
+{$ifdef FPC}
+{$mode DELPHI}
+{$else}
+{$apptype CONSOLE}
+{$endif}
+
+uses
+  SysUtils, Classes, fpcjs;
+
+var
+  JSRuntime: TFpcJsRuntime;
+  JSScript: TFpcJsScript;
+  cmd : string;
+  parsed : boolean;
+
+begin
+  JSRuntime := TFpcJsRunTime.Create();
+  JSScript := JSRuntime.CreateScript();
+  // include main library jc.js
+  if FileExists('jc.js') then
+    if not JSScript.EvaluateFile('jc.js') then
+      writeln('error: cannot evaluate implicit library "jc.js"');
+  // main loop
+  repeat
+    readln(cmd);
+    cmd := trim(cmd);
+    // ignore empty lines
+    if cmd='' then
+      continue;
+    // function declaration cannot be don via echo
+    if pos('function ',cmd) = 1 then
+    begin
+      parsed := JSScript.Evaluate(cmd);
+      if not parsed then
+        writeln('error: cannot evaluate script')
+      else
+        writeln('[function]');
+      continue;
+    end;
+    // everything else pass through echo
+    parsed := JSScript.Evaluate('echo('+cmd+',"\n");');
+    if not parsed then
+      writeln('error: cannot evaluate script');
+  until (cmd='q')or(cmd='quit')or(cmd='exit');
+end.
