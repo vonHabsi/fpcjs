a2f18e48adfab1f771680b53fc39224452aad90c
Dusan.Halicky <Dusan.Halicky@15cd7b3e-bbfe-11de-ad03-79fce359684b>
2010-12-08 10:44:23 +0000

    added jsh demo

---
+++ trunk/src/jsh.dpr
@@ -0,0 +1,132 @@
+program jsh;
+
+{*
+Experimental JavaScript command line shell
+
+Sample data file:
+
+object QRLabel1: TQRLabel
+  Left = 336
+  Top = 264
+end
+object QRLabel2: TQRLabel
+  Left = 336
+  Top = 264
+end
+object QRLabel3: TQRLabel
+  Left = 336
+  Top = 264
+end
+
+Bash script:
+
+cat zzz | grep object | sed 's/:/\n/;s/ /\n/' | grep qrl > zzz2
+for i in `cat zzz2`; do echo "  $i.Caption := inttostr(a[][]);"; done
+
+Desired output:
+
+  QRLabel1.Caption := inttostr(a[][]);
+  QRLabel2.Caption := inttostr(a[][]);
+  QRLabel3.Caption := inttostr(a[][]);
+
+JS Pseudocode
+
+  cat zzz       - load file or url
+  grep object
+  ...  
+
+}
+
+{$ifdef FPC}
+{$mode DELPHI}
+{$else}
+{$apptype CONSOLE}
+{$endif}
+
+uses
+  SysUtils, Classes, Variants, fpcjs, js15decl;
+
+var
+  JSRuntime: TFpcJsRuntime;
+  JSScript: TFpcJsScript;
+  cmd : string;
+  parsed : boolean;
+
+function jsh_cat(cx: PJSContext; obj: PJSObject; argc: uintN; argv, rval: pjsval): JSBool; cdecl;
+{*
+Load multiple files or URLs into array, probably separated by EOL
+}
+var i : integer;
+    params : TVariantArray;
+    s : string;
+begin
+  s := '';
+  params := JsParams(cx,obj,argc,argv);
+  for i := 0 to high(params) do
+  begin
+    // check for string
+    if not VarIsStr(params[i]) then
+    begin
+      writeln('error: jsh_cat param #',i,' is not string, filename required, skipped');
+      continue;
+    end;
+    // check if file exists, load it
+    if FileExists(params[i]) then
+    begin
+      with TStringList.Create do
+      try
+        LoadFromFile(params[i]);
+        s := s + Text;
+      finally
+        Free;
+      end;
+    end;
+  end;
+  result := JsResult(s,cx,rval,@params);;
+end;
+
+begin
+  JSRuntime := TFpcJsRunTime.Create();
+  JSScript := JSRuntime.CreateScript();
+
+  // include main library jsh.js
+  if FileExists('jsh.js') then
+    if not JSScript.EvaluateFile('jsh.js') then
+      writeln('error: cannot evaluate implicit library "jsh.js"');
+
+  // bind special functions
+  JS_DefineFunction(JSScript.cx, JSScript.gl, 'jsh_cat', @jsh_cat, 0, JSPROP_ENUMERATE or JSPROP_EXPORTED);
+
+  // parse testing script
+  parsed := JSScript.Evaluate('echo("aaa\n");  var s = jsh_cat(''sample.txt''); echo("s = ",s," lines=",s.split("\n").length,"\n");');
+  if not parsed then
+    writeln('error: cannot evaluate script');
+
+  writeln;
+  writeln('DONE');
+  readln;
+
+  // main loop
+{  repeat
+    readln(cmd);
+    cmd := trim(cmd);
+    // ignore empty lines
+    if cmd='' then
+      continue;
+    // function declaration cannot be don via echo
+    if pos('function ',cmd) = 1 then
+    begin
+      parsed := JSScript.Evaluate(cmd);
+      if not parsed then
+        writeln('error: cannot evaluate script')
+      else
+        writeln('[function]');
+      continue;
+    end;
+    // everything else pass through echo
+    parsed := JSScript.Evaluate('echo(indent,'+cmd+',suffix);');
+    if not parsed then
+      writeln('error: cannot evaluate script');
+  until (cmd='q')or(cmd='quit')or(cmd='exit');}
+end.
+
